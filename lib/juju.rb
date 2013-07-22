6963f7fc9dbd686d9b2dfbc1bb41f8f249d76b26
Elena Burachevskaya <alena.buracheuskaya@gmail.com>
2013-07-22 13:01:58 +0300

    Remove tags and spaces from response

--- lib/juju.rb
+++ lib/juju.rb
@@ -14,30 +14,34 @@ class Juju
   
   def search(params)
     check_required params
-    begin
-      response = RestClient.get URL, {:params => params}
-    rescue RestClient::BadRequest
-      raise JujuError, 'Request failed: invalid parameters'
-    end
+    response = RestClient.get URL, {:params => params}
     parse_xml response
+  rescue RestClient::BadRequest
+    raise JujuError, 'Request failed: invalid parameters'
   end
    
 protected
   def parse_xml(xml)
-    begin
-      data = Hash.from_xml(xml)['rss']['channel']
-    rescue REXML::ParseException
-      raise JujuError, 'Did not get a valid XML response from Juju'
-    end
-    jobs = data['item'].each {|item| item['link'].squish!}
+    data = Hash.from_xml(xml)['rss']['channel']
+    jobs = remove_tags_and_spaces data['item']
     JujuResult.new(jobs, data['totalresults'], data['startindex'], data['itemsperpage'])
+  rescue REXML::ParseException
+    raise JujuError, 'Did not get a valid XML response from Juju'
+  end
+
+  def remove_tags_and_spaces(jobs)
+    jobs.each do |item|
+      item['link'].squish!
+      item['title'] = strip_tags(item['title']).squish!
+      item['description'] = strip_tags(item['description']).squish!
+    end
   end
   
   def check_required(params)
-    raise JujuError.new('Partner ID was not provided') unless params[:partnerid]
-    raise JujuError.new('IP Address was not provided') unless params[:ipaddress] 
-    raise JujuError.new('User-Agent was not provided') unless params[:useragent]
-    raise JujuError.new('Required parameters were not provided') unless params[:k] || params[:l] || params[:c]
+    raise ArgumentError.new('Partner ID was not provided') unless params[:partnerid]
+    raise ArgumentError.new('IP Address was not provided') unless params[:ipaddress] 
+    raise ArgumentError.new('User-Agent was not provided') unless params[:useragent]
+    raise ArgumentError.new('Required parameters were not provided') unless params[:k] || params[:l] || params[:c]
   end
 
 end
--- lib/juju/version.rb
+++ lib/juju/version.rb
@@ -1,4 +1,4 @@
 module Juju
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
 
